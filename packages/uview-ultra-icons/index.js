48fe615b283071005c8b29744749f002fc7b4c0c
Phill.Lv <Phill.Lv@fanruan.com>
2026-03-05 14:42:47 +0800

    build: Configure icon package to use `dist` directory for main and module exports, removing the original `index.js` file.

--- packages/uview-ultra-icons/index.js
+++
@@ -1,3 +0,0 @@
-export { default as IconCloseCircle } from './components/CloseCircle.vue';
-export { default as IconHome } from './components/Home.vue';
-export { default as IconSuccessColor } from './components/SuccessColor.vue';
