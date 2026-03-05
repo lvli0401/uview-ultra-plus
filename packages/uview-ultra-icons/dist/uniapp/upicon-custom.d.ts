9e39df7b150f64f7384c12570decc556298fafef
Phill.Lv <Phill.Lv@fanruan.com>
2026-03-05 17:04:53 +0800

    chore(icons): update SVG assets via manager

--- packages/uview-ultra-icons/dist/uniapp/upicon-custom.d.ts
+++ packages/uview-ultra-icons/dist/uniapp/upicon-custom.d.ts
@@ -3,7 +3,8 @@ export enum UpiconCustom {
   Home = "upicon-custom-home",
   右箭头 = "upicon-custom-右箭头",
   店铺 = "upicon-custom-店铺",
+  开通服务 = "upicon-custom-开通服务",
 }
-export type UpiconCustomClassname = "upicon-custom-close-circle" | "upicon-custom-home" | "upicon-custom-右箭头" | "upicon-custom-店铺"
-export type UpiconCustomIcon = "close-circle" | "home" | "右箭头" | "店铺"
+export type UpiconCustomClassname = "upicon-custom-close-circle" | "upicon-custom-home" | "upicon-custom-右箭头" | "upicon-custom-店铺" | "upicon-custom-开通服务"
+export type UpiconCustomIcon = "close-circle" | "home" | "右箭头" | "店铺" | "开通服务"
 export const UpiconCustomPrefix = "upicon-custom-"
