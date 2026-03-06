0a17b9be2e2584c854229c75f7aa2b8a5c6b67d8
Phill.Lv <Phill.Lv@fanruan.com>
2026-03-06 15:22:19 +0800

    fix: remove path

---
+++ packages/components/ui/index.d.ts
@@ -0,0 +1,14 @@
+import { setConfig, install, $u } from './types/index'
+
+declare const _default: {
+  install: typeof install
+}
+
+export default _default
+
+export {
+  setConfig,
+  $u
+}
+
+export * from './types/index'
