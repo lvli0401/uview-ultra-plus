ad803d2426839227c64b353cdf023a35501827da
Phill.Lv <Phill.Lv@fanruan.com>
2026-03-06 14:48:40 +0800

    fix: remove packages/uview-ultra-icons/dist

--- packages/uview-ultra-icons/dist/index.js
+++
@@ -1,6 +0,0 @@
-export { default as IconCloseCircle } from './vue/CloseCircle.vue';
-export { default as IconHome } from './vue/Home.vue';
-export { default as IconSuccessColor } from './vue/SuccessColor.vue';
-export { default as Icon右箭头 } from './vue/右箭头.vue';
-export { default as Icon店铺 } from './vue/店铺.vue';
-export { default as Icon开通服务 } from './vue/开通服务.vue';
